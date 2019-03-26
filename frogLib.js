7e0e9d107a18cb91461033bfe7830a1708965a58
anunda51 <42669270+anunda51@users.noreply.github.com>
2019-03-26 11:57:00 +0700

    first commit

---
+++ frogLib.js
@@ -0,0 +1,47 @@
+function Fly() {
+    tFly = new Sprite(scene, "fly.png", 30, 30);
+    tFly.setSpeed(10);
+    tFly.wriggle = function () {
+        newDir = (Math.random() * 90) - 45;
+        this.changeAngleBy(newDir);
+    }
+    tFly.reset = function(){
+        newX = Math.random() * this.cWidth;
+        newY = Math.random() * this.cHeight;
+        this.setPosition(newX, newY);
+    }
+    tFly.reset();
+    return tFly;
+}
+
+function Frog() {
+    tFrog = new Sprite(scene, "frog.png", 50, 50);
+    tFrog.setSpeed(0);
+    tFrog.setAngle(0);
+    tFrog.maxSpeed = 10;
+    tFrog.minSpeed = -3;
+    tFrog.moveKey = function () {
+        if (keysDown[K_LEFT]) {
+            tFrog.changeAngleBy(-5);
+        }
+
+        if (keysDown[K_RIGHT]) {
+            tFrog.changeAngleBy(5);
+        }
+
+        if (keysDown[K_UP]) {
+            this.changeSpeedBy(1);
+            if (this.speed > this.maxSpeed) {
+                this.setSpeed(this.maxSpeed)
+            }
+        }
+
+        if (keysDown[K_DOWN]) {
+            this.changeSpeedBy(-1);
+            if (this.speed < this.minSpeed) {
+                this.setSpeed(this.minSpeed)
+            }
+        }
+    }
+    return tFrog;
+}
